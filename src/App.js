fda79b5faa54f0accbc37c2aeedb550c9695cf9e
subhashish negi <subhashish.negi@dror.co.in>
2020-08-12 01:14:37 +0530

    added header

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import  HomePage  from './pages/homepage/homepage.component.jsx';
 import './pages/homepage/homepage.styles.scss';
 import {Switch, Route } from 'react-router-dom';
 import  ShopPage  from './pages/shop/shop.component.jsx';
+import Header from './Components/header/header.component.jsx'
 
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage}></Route>
         <Route  path='/shop' component={ShopPage}></Route>
---
+++ src/Components/header/header.component.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './header.styles.scss';
+import { ReactComponent as Logo } from '../../logo.svg'
+
+const Header = ()=>{
+  return (
+    <div className="header">
+      <Link className="logo-container" to="/">
+        <Logo className="logo"></Logo>
+      </Link>
+      <div className="options">
+        <Link className="option" to="/shop">
+         Shop
+        </Link>
+        <Link  className="option">
+          CONTACT
+       </Link>
+      </div>
+    </div>
+  )
+}
+export default Header;
